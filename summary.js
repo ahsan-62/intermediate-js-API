3e6d26a4bbca87be61b8997c0e388d0551a95173
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-05-05 18:09:57 +0600

    summary with Template String ,Arrow Function ,Spread Operator

--- summary.js
+++ summary.js
@@ -1,6 +1,6 @@
 
 
-//problem -1
+//problem -1 - Write a template string that concatenates the strings "I am a web developer.", "I love to code.", and "I love to eat."
 
 const first='I am a web developer.';
 const second='I love to code.';
@@ -10,14 +10,14 @@ const res=`${first}${second}${third}`;
 console.log(res)
 
 
-//problem -2
+//problem -2 - Write a function that takes two parameters, a and b, and returns their sum.
 
 const sum=(a,b=5)=>a+b;
 const result= sum(5);
 console.log('The sum of a and b is: ',result);
 
 
-//problem -3
+//problem -3 - Take an array of names and return an array of only the names that have an even number of characters.
 
 const friends=['Ahmed','Sayed','Ali','Mahmud'];
 
@@ -33,7 +33,7 @@ const allFriends=(classFriend)=>{
 const result1 =allFriends(friends);
 console.log(result1);
 
-//Problem-4
+//Problem-4 -Take an array of numbers and square each number ,return the average of the numbers.
 
 const arr=[5,7,8,9];
 
@@ -54,12 +54,26 @@ const summ=(array)=>{
 const result2=summ(arr);
 console.log(result2);
 
-//Problem-5
+//Problem-5 -Take two arrays ,Merge into a new array and Find the maximum number.
 
-const arr1=[5,7,8,9];
-const arr2=[1,2,3,4];
 
 const fun=(array1,array2)=>{
+    const newArray1=array1;
+    const newArray2=array2;
+
+    const newArray=[];
+
+    newArray.push(...newArray1);
+    newArray.push(...newArray2);
+
+    const maximum=Math.max(...newArray)
+
+    return maximum;
     
 }
+
+const arr1=[10,21,32];
+const arr2=[14,25,26,];
+const result3=fun(arr1,arr2);
+console.log(result3);
 
