b4bf091b0f9bb884a2e575a98c48002d032e61b5
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-05-10 19:07:09 +0600

    Task 2 completed

--- class-object-constructor--inheritance/practice.js
+++ class-object-constructor--inheritance/practice.js
@@ -1,3 +1,5 @@
+//Task 01
+
 let data={
     sophia:{
         id:33,
@@ -19,3 +21,37 @@ let data={
 }
 
 console.log(data.sophia.study[1].secondary[1].location);
+
+
+//Task 2
+
+let students={
+    2222:{
+        name:'jack',
+        section:'A',
+        class:'10th',
+        address:{
+            building_no:123,
+            street:'abc',
+            city:'gabtoli',
+            country:'Bangladesh'
+        }
+
+    },
+    3333:{
+        name:'Herry',
+        section:'B',
+        class:'11th',
+        address:{
+            building_no:12333,
+            street:'asadabc',
+            city:'Dhanmondi',
+            country:'UK'
+        }
+
+    }
+}
+
+console.log(students[2222].address.city);
+
+console.log(students[3333].name);
