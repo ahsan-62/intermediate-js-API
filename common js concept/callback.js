0ce0afe8b595a30ab4f30c6d4fdef212638af6f4
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-05-12 00:14:05 +0600

    call back function

--- common js concept/callback.js
+++ common js concept/callback.js
@@ -1,10 +1,40 @@
 
-function ahsan(junFunction){
-    jun('Jun'); // ekhane abr jun function k call krlm ahsan function er moddhe .etai call back function
+
+/*
+function ahsan(junFunction,name){
+    jun(name); // ekhane abr jun function k call krlm ahsan function er moddhe .etai call back function
 }
 
 function jun(name){
     console.log('My Wife name is',name)
 }
 
-ahsan(jun); //jun function ta ahsan function er parameter hisebe pass korlam 
+ahsan(jun,'jun'); //jun function ta ahsan function er parameter hisebe pass korlam ,abr name parameter taw niye jaii
+
+
+*/
+
+
+//Call Back function
+
+function wishMode(modeFunction,name){
+
+    modeFunction(name);
+}
+
+function morning(name){
+    console.log('Good Morning',name);
+}
+
+function afternoon(name){
+    console.log('Good Afternoon',name);
+}
+
+function evening(name){ 
+    console.log('Good Evening',name);
+}
+
+wishMode(morning,'Ahsan');
+wishMode(afternoon,'Jun');
+wishMode(evening,'Bashar');
+wishMode(evening,'AhsanJun');
