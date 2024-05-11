92412f5b2058ee609b4b9597b6e8d3aaf245965d
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-05-11 23:56:06 +0600

    call back function

---
+++ common js concept/callback.js
@@ -0,0 +1,10 @@
+
+function ahsan(junFunction){
+    jun('Jun'); // ekhane abr jun function k call krlm ahsan function er moddhe .etai call back function
+}
+
+function jun(name){
+    console.log('My Wife name is',name)
+}
+
+ahsan(jun); //jun function ta ahsan function er parameter hisebe pass korlam 
